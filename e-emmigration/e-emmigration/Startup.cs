8a64c51ed7a74d3c8fe958a319b08a6fab95f8fe
Nasser <snasserahmed@gmail.com>
2018-01-16 18:57:58 +0530

    Initial project setup

---
+++ e-emmigration/e-emmigration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_emmigration.Startup))]
+namespace e_emmigration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
